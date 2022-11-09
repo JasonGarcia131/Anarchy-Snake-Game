4e3ba16484dff6231d69b273869d7c4d3b8c6fc9
Jason Garcia <jaygarcia0530@gmail.com>
2022-11-08 20:55:21 -0800

    added arrow buttons

--- input.js
+++ input.js
@@ -5,27 +5,28 @@ const down = document.getElementById('down');
 const left = document.getElementById('left');
 const right = document.getElementById('right');
 
-const buttonClicked = document.getElementsByClassName('arrows');
-
-buttonClicked.addEventListener('onClick', e => {
-  switch (e.id) {
-    case 'up':
-      if (lastInputDirection.y !== 0) break
+up.addEventListener("click", () => {
+      if (lastInputDirection.y !== 0) return
       inputDirection = { x: 0, y: -1 }
-      break
-    case 'down':
-      if (lastInputDirection.y !== 0) break
-      inputDirection = { x: 0, y: 1 }
-      break
-    case 'left':
-      if (lastInputDirection.x !== 0) break
-      inputDirection = { x: -1, y: 0 }
-      break
-    case 'right':
-      if (lastInputDirection.x !== 0) break
-      inputDirection = { x: 1, y: 0 }
-      break
-  }
+      return
+})
+
+down.addEventListener("click", ()=>{
+    if (lastInputDirection.y !== 0) return
+    inputDirection = { x: 0, y: 1 }
+    return
+})
+
+left.addEventListener("click", ()=>{
+    if (lastInputDirection.x !== 0) return
+    inputDirection = { x: -1, y: 0 }
+    return
+})
+
+right.addEventListener("click", ()=>{
+    if (lastInputDirection.x !== 0) return
+    inputDirection = { x: 1, y: 0 }
+    return
 })
 
 export function getInputDirection() {
