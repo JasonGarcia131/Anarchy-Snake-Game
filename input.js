4d3dc278e285a52827cb989648ad8306dafbaea5
Jason Garcia <jaygarcia0530@gmail.com>
2022-11-08 20:29:43 -0800

    added buttons for snake direction

--- input.js
+++ input.js
@@ -1,21 +1,27 @@
 let inputDirection = { x: 0, y: 0 }
 let lastInputDirection = { x: 0, y: 0 }
+const up = document.getElementById('up');
+const down = document.getElementById('down');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
 
-window.addEventListener('keydown', e => {
-  switch (e.key) {
-    case 'ArrowUp':
+const buttonClicked = document.getElementsByClassName('arrows');
+
+buttonClicked.addEventListener('onClick', e => {
+  switch (e.id) {
+    case 'up':
       if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: -1 }
       break
-    case 'ArrowDown':
+    case 'down':
       if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: 1 }
       break
-    case 'ArrowLeft':
+    case 'left':
       if (lastInputDirection.x !== 0) break
       inputDirection = { x: -1, y: 0 }
       break
-    case 'ArrowRight':
+    case 'right':
       if (lastInputDirection.x !== 0) break
       inputDirection = { x: 1, y: 0 }
       break
